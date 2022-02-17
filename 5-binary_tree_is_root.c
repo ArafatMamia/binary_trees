d6338287b4e3d7abdaa3812e92a628b13ddedcd7
Arafat Mamia <arafmamia@gmail.com>
2022-02-17 05:31:05 -0800

    binary tree is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -4,9 +4,10 @@
  * @node: pointer to node
  * Return: true or false
  */
+
 int binary_tree_is_root(const binary_tree_t *node)
 {
-        if (!(node) || node->parent)
-		return (0);
+if (node != NULL && node->parent == NULL)
 	return (1);
+return (0);
 }
